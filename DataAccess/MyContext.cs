5d4443a5ca3d9280772b486a297f4859d06e4888
Limantha <limanthafb@gmail.com>
2025-06-19 13:13:22 -0400

    - enabled code-first migration - added dbcontext class - did initial migration

---
+++ DataAccess/MyContext.cs
@@ -0,0 +1,18 @@
+﻿using DBNullExtensions.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace DBNullExtensions.DataAccess
+{
+    public class MyContext : DbContext
+    {
+        public MyContext() : base("DefaultConnection")
+        {
+        }
+        public DbSet<User> Users { get; set; }
+        
+    }
+}
